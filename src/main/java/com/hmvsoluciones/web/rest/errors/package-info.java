a38d3136eb520841fdb802b317331f1155bd0036
HMV Soluciones <hmv.soluciones@gmail.com>
2025-08-04 18:14:08 -0600

    Initial version of autolavadoSaas generated by generator-jhipster@8.11.0

---
+++ src/main/java/com/hmvsoluciones/web/rest/errors/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Rest layer error handling.
+ */
+package com.hmvsoluciones.web.rest.errors;
---
+++ src/test/java/com/hmvsoluciones/IntegrationTest.java
@@ -0,0 +1,20 @@
+package com.hmvsoluciones;
+
+import com.hmvsoluciones.config.AsyncSyncConfiguration;
+import com.hmvsoluciones.config.EmbeddedSQL;
+import com.hmvsoluciones.config.JacksonConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { AutolavadoSaasApp.class, JacksonConfiguration.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+public @interface IntegrationTest {
+}
